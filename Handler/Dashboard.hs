9147fe59ee9ec664a761cc2e80becfb1dfb367d7
Sam Lin <slin@Sams-MacBook-Pro.local>
2016-04-16 21:45:09 +1000

    Add dashboard Move newtoaustralia lucius to default

--- Handler/Dashboard.hs
+++ Handler/Dashboard.hs
@@ -2,7 +2,23 @@ module Handler.Dashboard where
 
 import Import
 
+data Mentor = M {
+      mName :: Text
+    , mImg  :: Text
+    , mDesc :: Text
+    }
 
+mentors :: [Mentor]
+mentors = [
+     M "Joe" "joe.png" "I'm experienced with working in local hospitals and with the adminstration of hospitals"
+    , M "Alice" "alice.png" "i am a doctor in melbourne"
+    , M "Bob" "bob.png" "i am a doctor in melbourne"
+    , M "Carlos" "carlos.png" "i am a doctor in melbourne"
+    , M "David" "david.png" "i am a doctor in melbourne"
+    , M "Edith" "edith.png" "i am a doctor in melbourne"
+    , M "Fred" "fred.png" "i am a doctor in melbourne"
+    , M "Georgia" "georgia.png" "i am a doctor in melbourne"
+  ]
 
 getDashboardR :: Handler Html
 getDashboardR = do
