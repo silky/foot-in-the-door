045a0e55a9a6265575ee1e25f7602add40235cc7
Sam Lin <slin@Sams-MacBook-Pro.local>
2016-04-16 16:33:09 +1000

    Localise homepage buttons

--- Foundation.hs
+++ Foundation.hs
@@ -23,6 +23,8 @@ data App = App
     , appLogger      :: Logger
     }
 
+mkMessage "App" "messages" "en"
+
 -- This is where we define all of the routes in our application. For a full
 -- explanation of the syntax, please see:
 -- http://www.yesodweb.com/book/routing-and-handlers
